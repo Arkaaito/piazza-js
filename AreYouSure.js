f71a28be37085f7bbe6cad323c7f266da8895b56
Arkaaito <arkaaito@scion-interactive.com>
2015-07-02 03:35:01 -0700

    Fixing this up after actually testing it.

--- AreYouSure.js
+++ AreYouSure.js
@@ -8,29 +8,35 @@
 // @grant        none
 // ==/UserScript==
 
-var guardSubmit = function() {
-        if ($('#post_button').length > 0 && typeof $('#post_button').data("events") !== undefined) {
-            var normalPost = $('#post_button').data("events");
+normalPost = null;
+
+var confirmSubmit = function() {
+    var isPublic = $('#entire_group').is(':checked');
+    var text = tinyMCE.get("rich_old_new_post").getContent().toLowerCase();
+    var saysPrivate = (text.indexOf("private") > -1 || text.indexOf("public") > -1 || text.indexOf("grade") > -1 || test.indexOf("deduction") > -1);
+    if (isPublic && saysPrivate) {
+        if (window.confirm("Your post may mention grade-related information, but is currently public.  Are you sure you want to post?")) {
             $('#post_button').off('click');
-            $('#post_button').on('click', function() {
-                var isPublic = $('#entire_group').is(':checked');
-                var text = tinyMCE.get("rich_old_new_post").getContent().toLowerCase();
-                var saysPrivate = (text.indexOf("private") > -1 || text.indexOf("public") > -1 || text.indexOf("grade") > -1 || test.indexOf("deduction") > -1);
-                if (isPublic && saysPrivate) {
-                    if (window.confirm("Your post may mention grade-related information, but is currently public.  Are you sure you want to post?")) {
-                        $('#post_button').off('click');
-                        $('#post_button').data("events", normalPost);
-                        $('#post_button').trigger('click');
-                    } else {
-                        $('#post_button').data("events", normalPost);
-                    }
-                } else {
-                    $('#post_button').off('click');
-                    $('#post_button').data("events", normalPost);
-                    $('#post_button').trigger('click');
-                }
+            $('#post_button').on('click', function(e) {
+                normalPost.handler();
             });
+            $('#post_button').trigger('click');
         }
+    } else {
+        $('#post_button').off('click');
+        $('#post_button').on('click', function(e) {
+            normalPost.handler();
+        });
+        $('#post_button').trigger('click');
+    }
+}
+
+var guardSubmit = function() {
+    if ($('#post_button').length > 0 && (typeof $('#post_button').data("events") !== "undefined") && $('#post_button').data("events").click[0].handler != confirmSubmit) {
+        normalPost = $('#post_button').data("events").click[0];
+        $('#post_button').off('click');
+        $('#post_button').on('click', confirmSubmit);
+    }
 };
 
 $(document).ready(function() {
